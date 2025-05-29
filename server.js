5c666225865c7ad04671785b1360eaf08929a7fd
WeWi <99915617+Webbernut@users.noreply.github.com>
2025-05-29 11:14:05 +0300

    server pc technology

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const {MongoClient} = require('mongodb')
+const app = express()
+require('dotenv').config()
+const MongoDBClient = new MongoClient(process.env.MONGO_URI)
+
+app.get('/', (req, res) => {
+    res.send('Сервер работает!');
+});
+
+app.get("/api/coveringType", async(req, res)=>{
+    try{
+        await MongoDBClient.connect()
+        console.log("успешное подключение")
+        const collection = MongoDBClient.db("catalog").collection("coveringType")
+        const coveringTypeItems = await collection.find({}).toArray()
+        res.status(200).json(coveringTypeItems)
+    }
+    catch(err){
+        res.status(500).json({ error: 'Ошибка сервера' });
+    }
+})
+
+app.listen(3000, () => {
+  console.log(`Сервер запущен на http://localhost:${3000}`);
+});
